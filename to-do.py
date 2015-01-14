09f1eba1e5590aaffbfe061f8514aa56bd1a220f
Anne <anne@annewoods.us>
2015-01-13 19:38:38 -0800

    beginning refactoring for draw question

--- to-do.py
+++ to-do.py
@@ -1,7 +1,7 @@
 '''
 
 DRILL
--
+- learned icon should show when there is one learned bird
 
 SETTINGS
 - add listen-only to drill settings
@@ -12,12 +12,16 @@ SETTINGS
 LOGOUT
 - login screen this redirects to does not accept login
 
+MIX-DETAIL
+- learned icon should show when there is one learned bird
+
 MIX EDIT
 - make only one popover open at time
 - make mix nickname link back to mix detail
 - make DELETE work
 - form validation
 - toggle grey checkboxes white on change
+
 
 FAVORITES
 - add unfavorite functionality to links
